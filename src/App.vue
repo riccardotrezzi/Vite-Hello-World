44dd0a2eebc897700d56bd39caf6f9f09c20859c
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-09-12 18:01:22 +0200

    Hello world completato

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,23 @@ Per importare ed utilizzare un componente dentro ad un altro devo SEMPRE seguire
 */
 
 //1. Importazione del componente
-import AppHeader from './components/AppHeader.vue';
 
 export default {
   data() {
     return { 
-      count: 0
-    }
-  },
-  // 2. Dichiarazione del componente
-  components: {
-    AppHeader
-  },
-  methods: {
-    incrementCount(){
-      this.count++;
+      message: 'Hello World!'
     }
   }
 }
 </script>
 
 <template>
-  <!--3. Utilizzo del componente-->
-  <AppHeader />
   <div>
-    <button @click="incrementCount()">
-      {{ count }}
-    </button>
+    <h1>
+      {{ message }}
+    </h1>
   </div>
 </template>
 
 <style scoped>
-
 </style>
